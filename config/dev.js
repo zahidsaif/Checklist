5624be5a5db7074b476a663424b806b48ea7de36
Zahid Saif <zahid.18dbca@cmr.edu.in>
2021-07-07 09:26:09 +0530

    Added Original Checklist files

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dbstring: 'mongodb://localhost:27017',
+    cookieKey: "random_string",
+    googleClientID: "801632878406-d9s00n0cse1dfu4rbfb2uuu2t4md1329.apps.googleusercontent.com",
+    googleSecretID: "OM0MHl505aXV9r1mBPTHE8y0",
+};
+
+
+
+Replace <password> with the password for the test user.
+    Replace myFirstDatabase with the name of the database that connections will use by default. Ensure any option params are URL encoded.
---
+++ routes/folder.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const {
+  postFolder,
+  getAllFolders,
+  getFolder,
+  updateFolder,
+  deleteFolder,
+} = require("./../controller/folder");
+const { protect } = require("./../controller/auth");
+
+const router = express.Router();
+
+router.route("/").post(protect, postFolder).get(protect, getAllFolders);
+
+router
+  .route("/:id")
+  .get(protect, getFolder)
+  .patch(protect, updateFolder)
+  .delete(protect, deleteFolder);
+
+module.exports = router;
